c8d98ed783a9d3778bdf17daff0e3ec8b012d96e
amitpseudo <amitbsharma.03@gmail.com>
2026-09-17 17:24:47 +0000

    add java ci pipeline

---
+++ src/main/java/app.java
@@ -0,0 +1,11 @@
+public class app {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps! Version 2");
+        System.out.println("2 + 3 = " + add(2, 3));
+    }
+}
---
+++ src/test/java/apptest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class apptest {
+
+    @Test
+    void testAddition() {
+        assertEquals(5, app.add(2, 3));
+    }
+}
